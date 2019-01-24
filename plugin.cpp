240c761e9f6cb58071a2ca20e4475002b7246129
Mark Riddoch <mark@dianomic.com>
2019-01-24 10:36:31 +0000

    FOGL-2338 Support external VERSION file

--- plugin.cpp
+++ plugin.cpp
@@ -14,6 +14,7 @@
 #include <config_category.h>
 #include <filter.h>
 #include <reading_set.h>
+#include <version.h>
 
 #define RULES "\\\"rules\\\" : []"
 #define FILTER_NAME "asset"
@@ -43,7 +44,7 @@ extern "C" {
  */
 static PLUGIN_INFORMATION info = {
         FILTER_NAME,              // Name
-        "1.0.0",                  // Version
+        VERSION,                  // Version
         0,                        // Flags
         PLUGIN_TYPE_FILTER,       // Type
         "1.0.0",                  // Interface version
